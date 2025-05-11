825cc6be3a6836ff919db3811b80b03db8d4f2de
Samuel H <sam-gh@mail.azran.eu>
2025-05-11 16:00:14 +0200

    add readlines-function and remove deprecated fasta handling

--- io_helpers.py
+++ io_helpers.py
@@ -1,6 +1,6 @@
 from typing import List, Tuple, Optional
 
-def read_file(
+def read_lines(
     filepath: str
 ) -> Optional[List[str]]:
     """
@@ -15,6 +15,29 @@ def read_file(
     try:
         with open(filepath, 'r') as file:
             content = file.readlines()
+            return content
+    except PermissionError:
+        print(f"You do not have permission to write the file at {filepath}.")
+    except IOError:
+        print("An I/O error occured.")
+    except Exception as e:
+        print(f"An unspecified error has occured while opening the file at {filepath}: \n {e}")
+
+def read_file(
+    filepath: str
+) -> Optional[List[str]]:
+    """
+    Read contents from a file
+
+    Args:
+        filepath (str): The path to the file from which should be read
+
+    Returns:
+        str or None: The file contents as string or None if there was an error reading from the file.
+    """
+    try:
+        with open(filepath, 'r') as file:
+            content = file.read()
             return content
     except PermissionError:
         print(f"You do not have permission to write the file at {filepath}.")
